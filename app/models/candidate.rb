3930e554a441aa1bed0c36aee34fb5473e000dc8
Shodhan G K <shodhu.17@gamil.com>
2020-01-06 00:08:20 +0530

    candidate validation added

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -4,4 +4,20 @@ class Candidate < ApplicationRecord
   belongs_to :user
   
   has_many :votes
+
+  validate  :already_enrolled, :already_enrolled_from_party
+  
+
+  def already_enrolled
+    if election.candidates.where(user_id: user_id).present?
+      errors.add(:user, 'already enrolled for this election')
+    end
+  end
+
+  def already_enrolled_from_party
+    participated_parties = election.candidates.pluck(:party_id)
+    if participated_parties.include? party_id
+      errors.add(:user, 'already enrolled from your party')
+    end
+  end
 end
